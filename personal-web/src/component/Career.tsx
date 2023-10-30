a5dcfe2c729445e8a0b0e5aa3a7dfa1e650cd0b4
joaquinarruiz <92789285+JoaquinArruiz@users.noreply.github.com>
2023-10-30 04:21:04 -0300

    added more animations

--- personal-web/src/component/Career.tsx
+++ personal-web/src/component/Career.tsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
 import '../styles/career.scss';
+import {motion} from 'framer-motion'
 
 const career =[
+  {
+    year: 2021,
+    month: "Dec",
+    title: 'Customer Support',
+    description: "During the Covid-19 pandemic's summers, I worked as a customer support agent for the Complejo E. Lotuf."
+  },
   {
     year: 2022,
     month: "June",
     title: 'Freelancer Designer',
     description: 'I worked as designer, doing logos, banners, and other stuff. Using Adobe Premiere, Adobe Illustrator and Photoshop.'
   },
-  {
-    year: 2023,
-    month: "Sep",
-    title: 'Odoo Developer',
-    description: 'I am currently working as an Odoo Developer at Elepe Servicios. Using Python, XML, PostgreSQL, Docker, among others.'
-  },
+  
   {
     year: 2023,
     month: "Sep",
@@ -30,7 +32,12 @@ const Career = () => {
   }
 
   return (
-    <div className="career section">
+    <motion.div  className="career section"
+    initial={{x: "100vw"}}
+    animate={{x: "0vw"}}
+    transition={{
+      delay: 1.9
+  }}>
       <h3>My Career Timeline</h3>
       <div className="career-display">
         <div className="timeline">
@@ -51,13 +58,17 @@ const Career = () => {
           <div className='timeline-items'> Present </div>
         </div>
         {selectedCircle !== null && (
+          
           <div className="selected-item">
-            <h4>{career[selectedCircle].title}</h4>
+            <h4>
+              {career[selectedCircle].title}
+            </h4>
+            <hr></hr>
             <p>{career[selectedCircle].description}</p>
           </div>
         )}
         </div>
-    </div>
+    </motion.div >
   );
 };
 
